f9f292a41666afa50b2a24595a8e6c46d3de31e4
Pablo Azevedo <pabloslayer@gmail.com>
2025-12-06 12:24:45 -0300

    hotfix(backend): Connect to vite frontend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import strawberry
 from strawberry.fastapi import GraphQLRouter
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 @strawberry.type
 class Query:
@@ -8,12 +10,25 @@ class Query:
     def hello(self) -> str:
         return "Pulse API is alive and beating."
 
-schema = strawberry.Schema(query=Query)
 
+schema = strawberry.Schema(query=Query)
 graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(graphql_app, prefix="/graphql")
+
 
 @app.get("/")
 def root():
