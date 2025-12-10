af899e02cc6ec132a5ffed110b365acad02b4778
Pablo Azevedo <pabloslayer@gmail.com>
2025-12-09 23:00:33 -0300

    feat(frontend): Update main page with authContext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,22 @@ import Navbar from './components/Navbar';
 import Login from './Login';
 import Register from './Register';
 import Home from './Home';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-pulse-black text-white font-sans">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider> 
+      <Router>
+        <div className="min-h-screen bg-pulse-black text-white font-sans">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
