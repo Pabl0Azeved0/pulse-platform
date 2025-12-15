e127130b0a9a7856dcff878a8ee4726643c94ac3
Pablo Azevedo <pabloslayer@gmail.com>
2025-12-15 20:20:18 -0300

    feat(backend): Add likes to models

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,8 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 import os
 
-# 1. Database Configuration
-# Using getenv ensures we pick up the Docker environment variable
 DATABASE_URL = os.getenv(
     "DATABASE_URL", "postgresql+asyncpg://pulse_user:pulse_password@db:5432/pulse_db"
 )
@@ -14,7 +12,6 @@ async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession
 Base = declarative_base()
 
 
-# 2. The User Model
 class User(Base):
     __tablename__ = "users"
 
@@ -28,7 +25,6 @@ class User(Base):
     posts = relationship("Post", back_populates="author")
 
 
-# 3. The Post Model
 class Post(Base):
     __tablename__ = "posts"
 
@@ -40,14 +36,21 @@ class Post(Base):
     author = relationship("User", back_populates="posts")
 
 
-# 4. Helper to init DB
+class Like(Base):
+    __tablename__ = "likes"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    post_id = Column(Integer, ForeignKey("posts.id"))
+
+    __table_args__ = (UniqueConstraint("user_id", "post_id", name="_user_post_uc"),)
+
+
 async def init_db():
     async with engine.begin() as conn:
         # This creates tables if they don't exist
         await conn.run_sync(Base.metadata.create_all)
 
 
-# 5. Dependency for Strawberry
 async def get_session():
     async with async_session() as session:
         yield session
