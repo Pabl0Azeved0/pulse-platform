ae74713086ea299b637a73c8f2f6ba03c05e4913
Pablo Azevedo <pabloslayer@gmail.com>
2025-12-31 16:11:56 -0300

    feat(frontend): Update apollo config using config file

--- frontend/src/apollo.ts
+++ frontend/src/apollo.ts
@@ -1,6 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { GRAPHQL_URL } from './config';
+
+const httpLink = createHttpLink({
+  uri: GRAPHQL_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
